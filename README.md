eb9e0a3494a289deb617715ff60ff9fa4ab7a88d
fwcd <fwcdmail@gmail.com>
2019-10-09 01:16:56 +0200

    Use proper package structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Deplocator
 A small tool that finds the Maven dependencies for a given set of Java source files by analyzing its import declarations.
+
+## Running
+`python3 -m deplocator [project folder]`
---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='deplocator',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'deplocator=deplocator:main'
+        ]
+    }
+)
