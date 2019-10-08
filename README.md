5b2ebe3958e8de7919506704fbfd02e98c161fdf
fwcd <fwcdmail@gmail.com>
2019-10-09 01:27:25 +0200

    Add project package prefix option
    Remove the hardcoded package prefix in favor of a configurable one.

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 A small tool that finds the Maven dependencies for a given set of Java source files by analyzing its import declarations.
 
 ## Running
-`python3 -m deplocator [project folder]`
+`python3 -m deplocator [project folder] [project package prefix]`
+
+For example, if you have a repository called `myapp` in your current directory with classes being located under `com.myapp`, you can use:
+
+`python3 -m deplocator myapp com.myapp`
--- deplocator/__main__.py
+++ deplocator/__main__.py
@@ -4,27 +4,27 @@ import re
 import urllib.request
 import json
 
-PACKAGE_REGEX = re.compile(r"import\s*(?:static)*\s*(.+)")
+IMPORT_REGEX = re.compile(r"import\s*(?:static)*\s*(.+)")
 
 def extract_package(import_line):
-    return PACKAGE_REGEX.match(import_line).group(1).rsplit(".", 1)[0]
+    return IMPORT_REGEX.match(import_line).group(1).rsplit(".", 1)[0]
 
-def is_external_package(line):
-    matched = PACKAGE_REGEX.match(line)
+def is_external_package(line, project_pkg_prefix):
+    matched = IMPORT_REGEX.match(line)
     if matched:
         pkg = matched.group(1)
-        return not pkg.startswith("java") and not pkg.startswith("javax") and not pkg.startswith("com.antelmann")
+        return not pkg.startswith("java") and not pkg.startswith("javax") and not pkg.startswith(project_pkg_prefix)
     return False
 
-def extract_imports(src_file_path):
+def extract_imports(src_file_path, project_pkg_prefix):
     try:
         with open(src_file_path, "r") as src_file:
-            return {extract_package(line) for line in src_file if is_external_package(line)}
+            return {extract_package(line) for line in src_file if is_external_package(line, project_pkg_prefix)}
     except UnicodeDecodeError:
         return set()
 
-def find_external_packages(base_path):
-    return {pkg for root, _, files in os.walk(base_path) for filename in files if filename.endswith(".java") for pkg in extract_imports(os.path.join(root, filename))}
+def find_external_packages(base_path, project_pkg_prefix):
+    return {pkg for root, _, files in os.walk(base_path) for filename in files if filename.endswith(".java") for pkg in extract_imports(os.path.join(root, filename), project_pkg_prefix)}
 
 def find_maven_dependency(package_name):
     with urllib.request.urlopen(r"http://search.maven.org/solrsearch/select?q=fc:%22" + package_name + r"%22&rows=20&wt=json") as raw_json:
@@ -32,11 +32,11 @@ def find_maven_dependency(package_name):
         return [dep["id"] for dep in results["response"]["docs"]]
 
 def main():
-    if len(sys.argv) < 2:
-        print("Pass a project folder as argument!")
+    if len(sys.argv) < 3:
+        print("Usage: deplocator [project folder] [project package prefix]")
     else:
         print("Packages:")
-        pkgs = find_external_packages(sys.argv[1])
+        pkgs = find_external_packages(sys.argv[1], sys.argv[2])
         print(pkgs)
         print("=============================")
         found = set()
